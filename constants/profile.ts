fc003a355c9cc283b4b0d2cb1ab848588d0dd99d
Nnadozi <chikaosro@gmail.com>
2026-04-23 00:24:55 -0400

    link

--- constants/profile.ts
+++ constants/profile.ts
@@ -6,7 +6,7 @@ export const PROFILE = {
   links: {
     github: "https://github.com/Nnadozi",
     linkedin: "https://www.linkedin.com/in/chikaosolu-nnadozie-798424279/",
-    cv: "https://drive.google.com/file/d/17U6lYjOR28ScPEiepEY-u1ZZhpzyxx2l/view?usp=sharing",
+    cv: "https://drive.google.com/file/d/15wH7Mt38RFFTiLRMW9CyvpEasBFhtHRw/view?usp=sharing",
     email: "mailto:chikaosro@gmail.com",
   },
 } as const;
